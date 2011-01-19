6f32fcb3888d694af120d9cf8ae9c45c1d9d1544
davyjones <innomotive@gmail.com>
2011-01-19 17:44:23 +0900

    Added table class.

---
+++ table.cpp
@@ -0,0 +1,129 @@
+#include "table.h"
+#include "schema.h"
+#include "mainWin.h"
+
+Table::Table(Schema* sch, QString tblName)
+{
+    int siz = sch->getTblList().size();
+    int i = sch->getTblList().findIndex(tblName);
+    int radius = 8 * siz;
+    if(radius < 100)
+        radius = 100;
+    qreal dtheta = 2*M_PI*i/siz;
+    this->setParent(sch);
+    this->setParentItem(sch);
+    this->setPos(radius*sin(dtheta),radius*cos(dtheta));
+    this->setName(tblName);
+    this->setStatus(false);
+    this->setCollapsed(true);
+    setFlag(QGraphicsItem::ItemIsMovable);
+    setFlag(QGraphicsItem::ItemIsSelectable);
+    setToolTip(this->getName());
+    setFlag(ItemSendsGeometryChanges);
+    setCacheMode(DeviceCoordinateCache);
+    setZValue(-1);
+}
+
+void Table::contextMenuEvent(QGraphicsSceneContextMenuEvent *event)
+{
+    QMenu menu;
+    menu.setStyleSheet("QMenu { font-size:12px; width: 100px; color:white; left: 20px; background-color:qlineargradient(x1:0, y1:0, x2:0, y2:1, stop: 0 #cccccc, stop: 1 #333333);}");
+    menu.addAction(tr("View"));
+    menu.addSeparator();
+    menu.addAction(tr("Delete"));
+    menu.addAction("Action 1");
+    QAction *a = menu.exec(event->screenPos());
+    if(a && QString::compare(a->text(),"Delete") == 0)
+    {
+        //this;
+    }
+}
+
+void Table::mouseDoubleClickEvent(QGraphicsSceneMouseEvent *)
+{
+    /*
+    if(this->getCollapsed())
+    {
+        this->setCollapsed(false);
+        this->view->show();;
+    }
+    else
+    {
+        this->setCollapsed(true);
+        this->view->raise();
+    }*/
+    /*
+    if(this->getCollapsed())
+    {
+        this->setCollapsed(false);
+        emit expand(this);
+    }
+    else
+    {
+        this->setCollapsed(true);
+        emit collapse(this);
+    }*/
+    emit expand(this->getParent(),this);
+    update();
+}
+
+void Table::showView(Schema* sch, Table *tbl)
+{
+    /*
+    QSqlTableModel* model = new QSqlTableModel;
+
+    // Construct proper tablename name for data
+    // retrieval as well as the titlebar.
+    QString tblName = sch->getName();
+    tblName.append(".");
+    tblName.append(tbl->getName());
+
+    model->setTable(tblName);
+    model->setEditStrategy(QSqlTableModel::OnManualSubmit);
+    model->select();
+
+    tbl->setModel(model);
+
+    View* view = new View(model, tblName, Qt::WA_DeleteOnClose);
+    */
+    LaunchTable* lt = new LaunchTable;
+    lt->start();
+    lt->showTbl(sch,tbl);
+}
+
+void Table::hideView(Schema*, Table*)
+{
+
+}
+
+bool Table::advance()
+{
+    if (newPos == pos())
+        return false;
+    setPos(newPos);
+    return true;
+}
+
+QVariant Table::itemChange(GraphicsItemChange change, const QVariant &value)
+{
+    switch (change) {
+    case ItemPositionHasChanged:
+        foreach (TableLink *edge, edgeList)
+            edge->adjust();
+        break;
+    default:
+        break;
+    };
+    return QGraphicsItem::itemChange(change, value);
+}
+
+void Table::addEdge(TableLink *edge)
+{
+    edgeList << edge;
+    edge->adjust();
+}
+
+QList<TableLink *> Table::edges() const
+{
+    return edgeList;
+}
---
+++ table.h
@@ -0,0 +1,150 @@
+#ifndef TABLE_H
+#define TABLE_H
+
+#include <QMouseEvent>
+#include <QSqlTableModel>
+#include <QtGui>
+#include "tableLink.h"
+
+class Table : public QObject, public QGraphicsItem
+{
+    Q_OBJECT
+    Q_INTERFACES(QGraphicsItem)
+
+private:
+    QSqlQueryModel* model;
+    Schema* schPar;
+    QString name;
+    bool status;
+    bool collapsed;
+    QList<TableLink *> edgeList;
+    QPointF newPos;
+protected:
+    QVariant itemChange(GraphicsItemChange change, const QVariant &value);
+public:
+    enum { Type = 10102 };
+    int type() const
+    {
+        return Type;
+    }
+    Table(Schema* parent, QString tblName);
+    ~Table(){};
+    bool advance();
+    void addEdge(TableLink *edge);
+    QList<TableLink *> edges() const;
+    QRectF boundingRect() const
+    {
+        return QRectF(-40,-20,85,30);
+    }
+    void paint(QPainter* painter, const QStyleOptionGraphicsItem* option,
+               QWidget* widget)
+    {
+        if(this->isSelected())
+            painter->setPen(QColor(100,50,50));
+        else
+            painter->setPen(QColor(0,0,0,0));
+        if(!this->getCollapsed()) {
+            painter->setBrush(QColor(220,220,220));
+            painter->drawRect(-40,-10,80,20);
+            painter->setBrush(QColor(200,200,200));
+            QPointF right[4] = {
+                 QPointF(+40,-10),
+                 QPointF(+40,+10),
+                 QPointF(+45,0),
+                 QPointF(+45,-20)
+             };
+            painter->drawPolygon(right, 4);
+            painter->setBrush(QColor(210,210,210));
+            QPointF top[4] = {
+                 QPointF(-30,-20),
+                 QPointF(-40,-10),
+                 QPointF(+40,-10),
+                 QPointF(+45,-20)
+             };
+            painter->drawPolygon(top, 4);
+            painter->setPen(Qt::darkGray);
+            QPointF textPos(-6*this->name.left(6).length(),+5);
+            painter->drawText(textPos, this->name.length()>6?this->name.left(6)+
+                              "..":this->name);
+        }
+        else {
+            painter->setBrush(QColor(200,150,150));
+            painter->drawRect(-40,-10,80,20);
+            painter->setBrush(QColor(230,200,200));
+            QPointF right[4] = {
+                QPointF(+40,-10),
+                QPointF(+40,+10),
+                QPointF(+45,0),
+                QPointF(+45,-20)
+             };
+            painter->drawPolygon(right, 4);
+            painter->setBrush(QColor(250,220,220));
+            QPointF top[4] = {
+                QPointF(-30,-20),
+                QPointF(-40,-10),
+                QPointF(+40,-10),
+                QPointF(+45,-20)
+             };
+            painter->drawPolygon(top, 4);
+            painter->setPen(QColor(100,50,50));
+            QPointF textPos(-6*this->name.left(6).length(),+5);
+            painter->drawText(textPos, this->name.length()>6?this->name.left(6)+
+                              "..":this->name);
+        }
+        painter->setRenderHint(QPainter::Antialiasing, true);
+    }
+    void showTable(Table*){};
+    QString getName()
+    {
+        return this->name;
+    }
+    void setName(QString name)
+    {
+        this->name = name;
+    }
+    bool getCollapsed()
+    {
+        return this->collapsed;
+    }
+    void setCollapsed(bool collapsed)
+    {
+        this->collapsed = collapsed;
+    }
+    bool getStatus()
+    {
+        return this->status;
+    }
+    void setStatus(bool status)
+    {
+        this->status = status;
+    }
+    void setParent(Schema* sch)
+    {
+        this->schPar = sch;
+    }
+    Schema* getParent()
+    {
+        return this->schPar;
+    }
+    QSqlQueryModel* getModel()
+    {
+        return this->model;
+    }
+    void setModel(QSqlQueryModel* model)
+    {
+        this->model = model;
+    }
+    virtual void mouseDoubleClickEvent(QGraphicsSceneMouseEvent *);
+    virtual void contextMenuEvent(QGraphicsSceneContextMenuEvent *);
+    void showView2(Table*);
+
+private slots:
+    void showView(Schema*,Table*);
+    void hideView(Schema*,Table*);
+
+Q_SIGNALS:
+    void expand(Schema*,Table*);
+    void collapse(Schema*,Table*);
+};
+
+#endif // TABLE_H
