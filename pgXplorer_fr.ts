d40733559d7b88b02f28fae372c5c1f3177aef0d
davyjones <innomotive@gmail.com>
2011-01-19 17:56:35 +0900

    Added translation files for French and Japanese.

---
+++ pgXplorer_fr.ts
@@ -0,0 +1,164 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr_FR">
+<defaultcodec>UTF-8</defaultcodec>
+<context>
+    <name>Connection</name>
+    <message>
+        <location filename="properties.cpp" line="123"/>
+        <location filename="properties.cpp" line="139"/>
+        <location filename="properties.cpp" line="160"/>
+        <source>Connection</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="properties.cpp" line="124"/>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="properties.cpp" line="125"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="properties.cpp" line="134"/>
+        <location filename="properties.cpp" line="155"/>
+        <source>Server</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="properties.cpp" line="135"/>
+        <location filename="properties.cpp" line="156"/>
+        <source>Database</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="properties.cpp" line="136"/>
+        <location filename="properties.cpp" line="157"/>
+        <source>Port</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="properties.cpp" line="137"/>
+        <location filename="properties.cpp" line="158"/>
+        <source>Username</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="properties.cpp" line="138"/>
+        <location filename="properties.cpp" line="159"/>
+        <source>Password</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Database</name>
+    <message>
+        <location filename="database.cpp" line="57"/>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="database.cpp" line="59"/>
+        <source>Properties</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="database.cpp" line="129"/>
+        <location filename="database.cpp" line="143"/>
+        <location filename="database.cpp" line="150"/>
+        <source>Database error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="database.cpp" line="130"/>
+        <source>Unable to establish a database connection.
+No PostgreSQL support.
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="database.cpp" line="144"/>
+        <source>Couldn&apos;t connect to database.
+Check connection parameters.
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="database.cpp" line="151"/>
+        <source>Unable to populate with schema(s).
+Contact database administrator.
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LaunchTable</name>
+    <message>
+        <location filename="mainWin.cpp" line="455"/>
+        <source>Query error</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWin</name>
+    <message>
+        <location filename="mainWin.cpp" line="115"/>
+        <location filename="mainWin.cpp" line="163"/>
+        <source>Open File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainWin.cpp" line="117"/>
+        <location filename="mainWin.cpp" line="165"/>
+        <source>Database (*.pgx)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PgxConsole</name>
+    <message>
+        <location filename="pgxconsole.cpp" line="11"/>
+        <source>SQL console</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="pgxconsole.cpp" line="252"/>
+        <source>WARNING: EXPLAIN ANAYZE for DML not supported at this time.
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QueryView</name>
+    <message>
+        <location filename="queryview.cpp" line="8"/>
+        <source>Time elapsed: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="queryview.cpp" line="9"/>
+        <source>Rows: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="queryview.cpp" line="10"/>
+        <source>Columns: </source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Table</name>
+    <message>
+        <location filename="table.cpp" line="40"/>
+        <source>View</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="table.cpp" line="42"/>
+        <source>Delete</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ pgXplorer_ja.ts
@@ -0,0 +1,164 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ja_JP">
+<context>
+    <name>Connection</name>
+    <message>
+        <location filename="properties.cpp" line="123"/>
+        <location filename="properties.cpp" line="139"/>
+        <location filename="properties.cpp" line="160"/>
+        <source>Connection</source>
+        <translation>連関</translation>
+    </message>
+    <message>
+        <location filename="properties.cpp" line="124"/>
+        <source>OK</source>
+        <translation>はい</translation>
+    </message>
+    <message>
+        <location filename="properties.cpp" line="125"/>
+        <source>Cancel</source>
+        <translation>キャンセル</translation>
+    </message>
+    <message>
+        <location filename="properties.cpp" line="134"/>
+        <location filename="properties.cpp" line="155"/>
+        <source>Server</source>
+        <translation>サーバー</translation>
+    </message>
+    <message>
+        <location filename="properties.cpp" line="135"/>
+        <location filename="properties.cpp" line="156"/>
+        <source>Database</source>
+        <translation>データベース</translation>
+    </message>
+    <message>
+        <location filename="properties.cpp" line="136"/>
+        <location filename="properties.cpp" line="157"/>
+        <source>Port</source>
+        <translation>ポート</translation>
+    </message>
+    <message>
+        <location filename="properties.cpp" line="137"/>
+        <location filename="properties.cpp" line="158"/>
+        <source>Username</source>
+        <translation>ユーザー名</translation>
+    </message>
+    <message>
+        <location filename="properties.cpp" line="138"/>
+        <location filename="properties.cpp" line="159"/>
+        <source>Password</source>
+        <translation>パスワード</translation>
+    </message>
+</context>
+<context>
+    <name>Database</name>
+    <message>
+        <location filename="database.cpp" line="57"/>
+        <source>Remove</source>
+        <translation>削除</translation>
+    </message>
+    <message>
+        <location filename="database.cpp" line="59"/>
+        <source>Properties</source>
+        <translation>プロパティー</translation>
+    </message>
+    <message>
+        <location filename="database.cpp" line="129"/>
+        <location filename="database.cpp" line="143"/>
+        <location filename="database.cpp" line="150"/>
+        <source>Database error</source>
+        <translation>データベースエラー</translation>
+    </message>
+    <message>
+        <location filename="database.cpp" line="130"/>
+        <source>Unable to establish a database connection.
+No PostgreSQL support.
+</source>
+        <translation>データベース接続は出来ませんでした。PostgreSQLドライバサポートは有りません。</translation>
+    </message>
+    <message>
+        <location filename="database.cpp" line="144"/>
+        <source>Couldn&apos;t connect to database.
+Check connection parameters.
+</source>
+        <translation>データベース接続は出来ませんでした。連関値を確認して下さい。</translation>
+    </message>
+    <message>
+        <location filename="database.cpp" line="151"/>
+        <source>Unable to populate with schema(s).
+Contact database administrator.
+</source>
+        <translation>スキーマ検束は出来ませんでした。管理者まで連絡下さい。</translation>
+    </message>
+</context>
+<context>
+    <name>LaunchTable</name>
+    <message>
+        <location filename="mainWin.cpp" line="455"/>
+        <source>Query error</source>
+        <translation>クエリーエラー。</translation>
+    </message>
+</context>
+<context>
+    <name>MainWin</name>
+    <message>
+        <location filename="mainWin.cpp" line="115"/>
+        <location filename="mainWin.cpp" line="163"/>
+        <source>Open File</source>
+        <translation>ファイル開く</translation>
+    </message>
+    <message>
+        <location filename="mainWin.cpp" line="117"/>
+        <location filename="mainWin.cpp" line="165"/>
+        <source>Database (*.pgx)</source>
+        <translation>PostgreSQLデータベース (*.pgx)</translation>
+    </message>
+</context>
+<context>
+    <name>PgxConsole</name>
+    <message>
+        <location filename="pgxconsole.cpp" line="11"/>
+        <source>SQL console</source>
+        <translation>SQLコンソール</translation>
+    </message>
+    <message>
+        <location filename="pgxconsole.cpp" line="252"/>
+        <source>WARNING: EXPLAIN ANAYZE for DML not supported at this time.
+</source>
+        <translation>WARNING: この度DMLの「EXPLAIN ANALYZE」のサポートは有りません。
+</translation>
+    </message>
+</context>
+<context>
+    <name>QueryView</name>
+    <message>
+        <location filename="queryview.cpp" line="8"/>
+        <source>Time elapsed:</source>
+        <translation>時間：</translation>
+    </message>
+    <message>
+        <location filename="queryview.cpp" line="9"/>
+        <source>Rows:</source>
+        <translation>行数：</translation>
+    </message>
+    <message>
+        <location filename="queryview.cpp" line="10"/>
+        <source>Columns:</source>
+        <translation>欄数：</translation>
+    </message>
+</context>
+<context>
+    <name>Table</name>
+    <message>
+        <location filename="table.cpp" line="40"/>
+        <source>View</source>
+        <translation>ビュー</translation>
+    </message>
+    <message>
+        <location filename="table.cpp" line="42"/>
+        <source>Delete</source>
+        <translation>削除</translation>
+    </message>
+</context>
+</TS>
